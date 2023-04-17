a6eaeb04811c00a69c43dd16a1a5798a4f93bba3
Petri-Johan Last <petri.last@sourcegraph.com>
2023-04-17 17:56:09 +0200

    Simplify GetRepoName test

--- providers/sourcegraph_test.go
+++ providers/sourcegraph_test.go
@@ -3,36 +3,18 @@ package providers
 import "testing"
 
 func TestGetRepoName(t *testing.T) {
-	testCases := []struct {
-		gitURL string
-		want   string
-	}{
-		{
-			gitURL: "git@github.com:sourcegraph/sourcegraph.git",
-			want:   "github.com/sourcegraph/sourcegraph",
-		},
-		{
-			gitURL: "https://github.com/sourcegraph/sourcegraph.git",
-			want:   "github.com/sourcegraph/sourcegraph",
-		},
-		{
-			gitURL: "git://github.com/sourcegraph/sourcegraph.git",
-			want:   "github.com/sourcegraph/sourcegraph",
-		},
-		{
-			gitURL: "http://github.com/sourcegraph/sourcegraph.git",
-			want:   "github.com/sourcegraph/sourcegraph",
-		},
-		{
-			gitURL: "https://username:password@github.com/sourcegraph/sourcegraph.git",
-			want:   "github.com/sourcegraph/sourcegraph",
-		},
+	want := "github.com/sourcegraph/sourcegraph"
+	urls := []string{
+		"git@github.com:sourcegraph/sourcegraph.git",
+		"https://github.com/sourcegraph/sourcegraph.git",
+		"git://github.com/sourcegraph/sourcegraph.git",
+		"http://github.com/sourcegraph/sourcegraph.git",
+		"https://username:password@github.com/sourcegraph/sourcegraph.git",
 	}
-
-	for _, tc := range testCases {
-		got := getRepoName(tc.gitURL)
-		if got != tc.want {
-			t.Errorf("getRepoName(%q) == %q, want %q", tc.gitURL, got, tc.want)
+	for _, url := range urls {
+		got := getRepoName(url)
+		if got != want {
+			t.Errorf("getRepoName(%q) == %q, want %q", url, got, want)
 		}
 	}
 }
