40cb3801b92d2d16e41bcc6dccb9752bd2188894
Petri-Johan Last <petri.last@sourcegraph.com>
2023-05-05 08:01:56 +0200

    Move version tag to after llmsp

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 TAG=$(git describe --tags --always)
 
-env GOOS=darwin GOARCH=arm64 go build -o build/llmsp-arm64-darwin-${TAG} .
-env GOOS=darwin GOARCH=amd64 go build -o build/llmsp-amd64-darwin-${TAG} .
-env GOOS=linux GOARCH=amd64 go build -o build/llmsp-amd64-linux-${TAG} .
-env GOOS=linux GOARCH=arm64 go build -o build/llmsp-arm64-linux-${TAG} .
-env GOOS=windows GOARCH=amd64 go build -o build/llmsp-amd64-windows-${TAG}.exe .
+env GOOS=darwin GOARCH=arm64 go build -o build/llmsp-${TAG}-arm64-darwin .
+env GOOS=darwin GOARCH=amd64 go build -o build/llmsp-${TAG}-amd64-darwin .
+env GOOS=linux GOARCH=amd64 go build -o build/llmsp-${TAG}-amd64-linux .
+env GOOS=linux GOARCH=arm64 go build -o build/llmsp-${TAG}-arm64-linux .
+env GOOS=windows GOARCH=amd64 go build -o build/llmsp-${TAG}-amd64-windows.exe .
